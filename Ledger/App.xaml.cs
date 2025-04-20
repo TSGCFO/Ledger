84a42e8e60f17437b8211f64043f2407e4a9e04f
TSGCFO <h.sadiq@tsgfulfillment.com>
2025-04-19 23:16:30 -0400

    Add project files.

---
+++ Ledger/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Ledger
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
